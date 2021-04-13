1b648f0714292c7f23da48276c0d86566a41a2f4
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-04-13 22:35:23 +0100

    feat: implement drawing of hand keypoints

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
 let cam;
 let handpose;
+let keypoints = [];
 
 function drawCam() {
   translate(600, 0);
   scale(-1, 1);
   image(cam, 0, 0, 600, 450);
+}
+
+function drawKeypoints() {
+  for (const keypoint of keypoints) {
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(keypoint.x, keypoint.y, 10, 10);
+  }
 }
 
 function setup() {
@@ -15,9 +24,20 @@ function setup() {
   handpose = ml5.handpose(cam, () => {
     console.log('Handpose model loaded.')
   });
+  handpose.on('predict', gotHand);
+}
+
+function gotHand(results) {
+  if (results.length == 0) {
+    keypoints = [];
+    return;
+  }
+  const { landmarks } = results[0];
+  keypoints = landmarks.map(landmark => ({ x: landmark[0], y: landmark[1] }));
 }
 
 function draw() {
   background(220);
   drawCam();
+  drawKeypoints();
 }
