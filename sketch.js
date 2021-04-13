5aa0323f57062801e2909f8ef4ed34e209ef53d5
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-04-13 21:50:40 +0100

    feat: implement camera capture

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let cam;
+
+function drawCam() {
+  translate(600, 0);
+  scale(-1, 1);
+  image(cam, 0, 0, 600, 450);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  cam = createCapture(VIDEO);
+  cam.hide();
 }
 
 function draw() {
   background(220);
+  drawCam();
 }
